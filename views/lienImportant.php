0185f8c6d6d358f2125046c6432580b6c5c2d93b
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-04-27 15:51:59 +0400

    Add random avatars for teamship in the "lienImportant" file.
    Add circle, and fake names.

--- views/lienImportant.php
+++ views/lienImportant.php
@@ -2,26 +2,29 @@
     <div class="section section-white">
       <div class="container">
         <div class="row">
+          <div class="section-title">
+            <h1>L'équipage</h1>
+          </div>
           <div class="col-md-4 col-sm-6">
             <div class="service-wrapper">
-              <i class="icon-home"></i>
-              <h3>Aliquam in adipiscing</h3>
+              <img class="img-circle img-thumbnail" src="https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" alt="">
+              <h3>Tom Mcpeak</h3>
               <p>Praesent rhoncus mauris ac sollicitudin vehicula. Nam fringilla turpis turpis, at posuere turpis aliquet sit amet condimentum</p>
               <a href="#" class="btn">Read more</a>
             </div>
           </div>
           <div class="col-md-4 col-sm-6">
             <div class="service-wrapper">
-              <i class="icon-briefcase"></i>
-              <h3>Curabitur mollis</h3>
+              <img class="img-circle img-thumbnail" src="https://s3.amazonaws.com/uifaces/faces/twitter/sauro/128.jpg" alt="">
+              <h3>Harry Forbes</h3>
               <p>Suspendisse eget libero mi. Fusce ligula orci, vulputate nec elit ultrices, ornare faucibus orci. Aenean lectus sapien, vehicula</p>
               <a href="#" class="btn">Read more</a>
             </div>
           </div>
           <div class="col-md-4 col-sm-6">
             <div class="service-wrapper">
-              <i class="icon-calendar"></i>
-              <h3>Vivamus mattis</h3>
+              <img class="img-circle img-thumbnail" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg" alt="">
+              <h3>Samwel Chernin</h3>
               <p>Phasellus posuere et nisl ac commodo. Nulla facilisi. Sed tincidunt bibendum cursus. Aenean vulputate aliquam risus rutrum scelerisque</p>
               <a href="#" class="btn">Read more</a>
             </div>
